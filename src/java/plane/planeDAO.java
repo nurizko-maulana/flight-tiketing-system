babbeeb433058994a735691f893213aa01e373c9
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-02-01 19:56:41 +0700

    ad login controller

---
+++ src/java/plane/planeDAO.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plane;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+/**
+ *
+ * @author duncanleo
+ */
+public class planeDAO {
+    
+}
